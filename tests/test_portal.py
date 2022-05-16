ed0258aed549b0db530e0ac59ec581ec1969934d
sanghyunlee <narophoa@gmail.com>
2022-05-16 22:46:34 +0900

    pytest

--- tests/test_portal.py
+++ tests/test_portal.py
@@ -3,26 +3,16 @@ from selenium import webdriver
 from locators.locators import HomepageLocators
 
 
-class TestWebzenPortal(unittest.TestCase):
-
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30)
+class TestPortal():
 
     def test_page_load_portal(self):
-        driver = self.driver
+        driver = webdriver.Chrome()
         driver.get(HomepageLocators.PATH)
-        title = self.driver.title
+        title = driver.title
         assert 'Webzen - Games Portal' in title
 
     def test_page_load_member(self):
-        driver = self.driver
+        driver = webdriver.Chrome()
         driver.get("http://member.webzen.co.kr")
-        title = self.driver.title
+        title = driver.title
         assert '로그인 - 웹젠' in title
-        
-    def tearDown(self):
-        self.driver.close()
-
-if __name__ == "__main__":
-    unittest.main()
